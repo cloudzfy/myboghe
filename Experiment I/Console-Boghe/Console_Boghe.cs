3004fddef6e20f79cd1b93a185b91671cee3fbbc
cloudzfy <cloudzfy@gmail.com>
2013-12-07 09:57:27 +0800

    Upload The First Experiment

---
+++ Experiment I/Console-Boghe/Console_Boghe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.doubango.tinyWRAP;
+using System.Timers;
+using System.Threading;
+namespace Console_Boghe
+{
+    class Console_Boghe
+    {
+        private RegistrationSession session;
+        private MySipCallback sipCallback;
+        private SipStack stack;
+        private MessagingSession mSession;
+        private String message;
+        private String name;
+
+        private String impu;
+        private String impi;
+        private String pwd;
+        private String realm = "sip:open-ims.test";
+        private String pcscf_Host = "192.168.16.110";
+        private String pcscf_Port = "4060";
+        private String call_address;
+        private Boolean isCall = false;
+
+        public Console_Boghe()
+        {
+        }
+
+        public void init()
+        {
+            System.Console.WriteLine("Please input the user name:");
+            name = System.Console.ReadLine();
+            impu = "sip:" + name + "@open-ims.test";
+            impi = name + "@open-ims.test";
+            System.Console.WriteLine("Please input the password:");
+            pwd = System.Console.ReadLine();
+            SipStack.initialize();
+            sipCallback = new MySipCallback();
+            stack = new SipStack(sipCallback, realm, impi, impu);
+            stack.setPassword(pwd);
+            stack.setAMF("0x0000");
+            stack.setOperatorId("0x00000000000000000000000000000000");
+            stack.setSTUNServer(null, 0);
+            stack.setProxyCSCF(pcscf_Host, (ushort)Convert.ToInt32(pcscf_Port), "UDP", "IPv4");
+            stack.addHeader("Allow", "INVITE, ACK, CANCEL, BYE, MESSAGE, OPTIONS, NOTIFY, PRACK, UPDATE, REFER");
+            stack.addHeader("Privacy", "none");
+            stack.addHeader("P-Access-Network-Info", "ADSL;utran-cell-id-3gpp=00000000");
+            stack.addHeader("User-Agent", String.Format("IM-client/OMA1.0 Boghe/v{0}", 
+                System.Reflection.Assembly.GetEntryAssembly().GetName().Version));
+            stack.start();
+
+            session = new RegistrationSession(stack);
+            session.setFromUri(impu);
+            if (session.register_())
+            {
+                isCall = false;
+            }
+            System.Console.WriteLine("Please input who you are going to chat with:");
+            name = System.Console.ReadLine();
+            call_address = "sip:" + name + "@open-ims.test";
+
+            if (!isCall)
+            {
+                mSession = new MessagingSession(stack);
+                mSession.addHeader("Content-Type", "text/plain");
+                mSession.setFromUri(impu);
+                mSession.setToUri(call_address);
+                isCall = true;
+                System.Console.WriteLine("you can talk now.(press q to exit)");
+            }
+            else
+            {
+                isCall = false;
+            }
+
+            while (true)
+            {
+                message = System.Console.ReadLine();
+                if (message == "q") break;
+                else
+                {
+                    byte[] payload = Encoding.UTF8.GetBytes(message);
+                    mSession.send(payload, (uint)payload.Length);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console_Boghe consoleBoghe = new Console_Boghe();
+            consoleBoghe.init();
+        }
+    }
+}
---
+++ Experiment I/Console-Boghe/MySipCallback.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.doubango.tinyWRAP;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Console_Boghe
+{
+    public class MySipCallback : SipCallback
+    {
+        public MySipCallback()
+        {
+        }
+
+        public override int OnMessagingEvent(MessagingEvent e)
+        {
+            tsip_message_event_type_t type = e.getType();
+            switch (type)
+            {
+                case tsip_message_event_type_t.tsip_ao_message:
+                    break;
+                case tsip_message_event_type_t.tsip_i_message:
+                    {
+                        SipMessage message = e.getSipMessage();
+                        MessagingSession session = e.getSession();
+                        uint sessionID;
+                        if (session == null)
+                        {
+                            session = e.takeSessionOwnership();
+                        }
+                        if (message == null)
+                        {
+                            session.reject();
+                            session.Dispose();
+                            return 0;
+                        }
+                        sessionID = session.getId();
+                        String name = message.getSipHeaderValue("f");
+                        name = name.Substring(4, name.IndexOf('@') - 4);
+                        byte[] bytes = message.getSipContent();
+                        if (bytes == null || bytes.Length == 0)
+                        {
+                            session.reject();
+                            session.Dispose();
+                            return 0;
+                        }
+                        session.accept();
+                        session.Dispose();
+                        Console.WriteLine(name + ": " + System.Text.Encoding.Default.GetString(bytes));
+                        break;
+                    }
+            }
+            return 0;
+        }
+
+        public override int OnRegistrationEvent(RegistrationEvent e)
+        {
+            tsip_register_event_type_t type = e.getType();
+            switch (type)
+            {
+                case tsip_register_event_type_t.tsip_ao_register: System.Console.WriteLine("(Registed success)"); break;
+                case tsip_register_event_type_t.tsip_i_newreg: break;
+                case tsip_register_event_type_t.tsip_i_register: break;
+                case tsip_register_event_type_t.tsip_i_unregister: break;
+                case tsip_register_event_type_t.tsip_ao_unregister:  break;
+            }
+            return 0;
+        }
+    }
+}
